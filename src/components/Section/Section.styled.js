65c246b0c1de02331500cf0e90d7f2c073f41ab4
Alina Demchenko <demalina88@gmail.com>
2023-07-06 21:32:37 -0700

    final 1

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,13 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledSection = styled.div`
-max-width: 1440px;
+width: 100%;
 margin: 0 auto;
 padding: 100px 15px 15px;
 color: #010101;
 background-color: #fbeffc;`;
-
-// 
-// display: flex;
-// justify-content: center;
-// align-items: center;
